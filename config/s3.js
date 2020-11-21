10f1b5f88ce28b388170758ae8add4dbc704a78e
Vitor Luis Fernandes <vitor-l-f@hotmail.com>
2020-11-20 21:10:00 -0300

    Removendo axios e utilizando dotenv

--- config/s3.js
+++ config/s3.js
@@ -1,12 +1,12 @@
 var AWS = require('aws-sdk');
 
 AWS.config.update({
-    region: 'us-east-1'
+    region: process.env.S3_REGION
 });
 
 var s3 = new AWS.S3({
-    accessKeyId: "",
-    secretAccessKey: ""
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
 });
 
 
